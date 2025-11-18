993473a024b67db2c6a1e2b35f6ea98cbdbab5e0
Mikk333k <mikkel@schorling.dk>
2025-11-18 10:58:40 +0100

    DELETE ALL

--- routes/orders.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const orderController = require('../controllers/orderController');
-const auth = require('../middleware/auth');  // sikrer login
-
-router.post('/', auth, orderController.createOrder);
-
-module.exports = router;
